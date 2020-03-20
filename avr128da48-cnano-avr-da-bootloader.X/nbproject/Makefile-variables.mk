1cf9468958c2776e8332fe2a45602c50abb46aa3
Marius Nicolae <Marius.Nicolae@microchip.com>
2020-03-20 17:06:32 +0200

    MPAE-2046 add bootloader project

---
+++ avr128da48-cnano-avr-da-bootloader.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Release configuration
+CND_ARTIFACT_DIR_Release=dist/Release/production
+CND_ARTIFACT_NAME_Release=avr128da48-cnano-avr-da-bootloader.X.production.hex
+CND_ARTIFACT_PATH_Release=dist/Release/production/avr128da48-cnano-avr-da-bootloader.X.production.hex
+CND_PACKAGE_DIR_Release=${CND_DISTDIR}/Release/package
+CND_PACKAGE_NAME_Release=avr128da48-cnano-avr-da-bootloader.x.tar
+CND_PACKAGE_PATH_Release=${CND_DISTDIR}/Release/package/avr128da48-cnano-avr-da-bootloader.x.tar
